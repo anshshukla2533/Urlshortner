9965e20fb272b7bca6f9fad57d20812070a53b49
Ansh Shukla <anshshuklavg>
2025-10-31 18:58:48 +0530

    contact

--- app/contact/page.js
+++ app/contact/page.js
@@ -5,7 +5,6 @@ export default function ContactPage() {
   return (
     <main className="min-h-screen bg-purple-100 text-gray-800 p-6">
       <div className="max-w-3xl mx-auto text-center">
-        {}
         <Image
           src="/ansh.jpg"
           alt="Ansh Shukla"
@@ -16,7 +15,7 @@ export default function ContactPage() {
 
         <h1 className="text-4xl font-bold mb-2">Contact Me</h1>
         <p className="text-lg mb-6 text-gray-600">
-          I'd love to hear from you! Whether it's about feedback, projects, or just a hello.
+          I&apos;d love to hear from you! Whether it&apos;s about feedback, projects, or just a hello.
         </p>
       </div>
 
